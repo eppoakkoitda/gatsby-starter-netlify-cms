f6a951610bcca38a4d4ae6cbd0baf5166ce911a6
eppoakkoitda <43262437+eppoakkoitda@users.noreply.github.com>
2019-10-31 15:21:57 +0900

    home

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -5,17 +5,21 @@ import { Link, graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import Features from '../components/Features'
 import BlogRoll from '../components/BlogRoll'
+import Content, { HTMLContent } from '../components/Content'
 
 export const IndexPageTemplate = ({
   image,
   title,
-  body,
   heading,
   subheading,
   mainpitch,
   description,
   intro,
-}) => (
+  contentComponent,
+  content
+}) => {
+  const PageContent = contentComponent || Content  
+  return (
   <div>
     <div
       className="full-width-image margin-top-0"
@@ -65,13 +69,14 @@ export const IndexPageTemplate = ({
         </h3>
       </div>
     </div>
+    <PageContent className="content" content={content} />
     <section className="section section--gradient">
       <div className="container">
         <div className="section">
           <div className="columns">
             <div className="column is-10 is-offset-1">
               <div className="content">
-                <div className="content">
+                {/* <div className="content">
                   <div className="tile">
                     <h1 className="title">{mainpitch.title}</h1>
                   </div>
@@ -79,28 +84,28 @@ export const IndexPageTemplate = ({
                     <h3 className="subtitle">{mainpitch.description}</h3>
                   </div>
                   <div className="tile">
-                    <h3 className="subtitle">{body}</h3>
+                    <h3 className="subtitle">あいうえお</h3>
                   </div>
-                </div>
-                <div className="columns">
+                </div> */}
+                {/* <div className="columns">
                   <div className="column is-12">
                     <h3 className="has-text-weight-semibold is-size-2">
                       {heading}
                     </h3>
                     <p>{description}</p>
                   </div>
-                </div>
-                <Features gridItems={intro.blurbs} />
-                <div className="columns">
+                </div> */}
+                {/* <Features gridItems={intro.blurbs} /> */}
+                {/* <div className="columns">
                   <div className="column is-12 has-text-centered">
                     <Link className="btn" to="/products">
                       See all products
                     </Link>
                   </div>
-                </div>
+                </div> */}
                 <div className="column is-12">
                   <h3 className="has-text-weight-semibold is-size-2">
-                    Latest stories
+                    最新の投稿
                   </h3>
                   <BlogRoll />
                   <div className="column is-12 has-text-centered">
@@ -116,12 +121,11 @@ export const IndexPageTemplate = ({
       </div>
     </section>
   </div>
-)
+)}
 
 IndexPageTemplate.propTypes = {
   image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
   title: PropTypes.string,
-  body: PropTypes.string,
   heading: PropTypes.string,
   subheading: PropTypes.string,
   mainpitch: PropTypes.object,
@@ -129,22 +133,26 @@ IndexPageTemplate.propTypes = {
   intro: PropTypes.shape({
     blurbs: PropTypes.array,
   }),
+  contentComponent: PropTypes.func,
+  content: PropTypes.string,
 }
 
 const IndexPage = ({ data }) => {
   const { frontmatter } = data.markdownRemark
+  const { markdownRemark: post } = data
 
   return (
     <Layout>
       <IndexPageTemplate
         image={frontmatter.image}
         title={frontmatter.title}
-        body={frontmatter.body}
         heading={frontmatter.heading}
         subheading={frontmatter.subheading}
         mainpitch={frontmatter.mainpitch}
         description={frontmatter.description}
         intro={frontmatter.intro}
+        contentComponent={HTMLContent}
+        content={post.html}
       />
     </Layout>
   )
@@ -163,6 +171,7 @@ export default IndexPage
 export const pageQuery = graphql`
   query IndexPageTemplate {
     markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
+      html
       frontmatter {
         title
         image {
