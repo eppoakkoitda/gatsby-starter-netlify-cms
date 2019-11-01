77b9defa7b2c0d73d1f5c6479479038941cc42f6
eppoakkoitda <43262437+eppoakkoitda@users.noreply.github.com>
2019-11-01 16:19:24 +0900

    next and pre

--- gatsby-node.js
+++ gatsby-node.js
@@ -21,6 +21,7 @@ exports.createPages = ({ actions, graphql }) => {
             }
           }
         }
+        
       }
     }
   `).then(result => {
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -5,6 +5,7 @@ import Helmet from 'react-helmet'
 import { graphql, Link } from 'gatsby'
 import Layout from '../components/Layout'
 import Content, { HTMLContent } from '../components/Content'
+//import { RefCountDisposable } from 'rx-core'
 
 export const BlogPostTemplate = ({
   content,
@@ -13,8 +14,19 @@ export const BlogPostTemplate = ({
   tags,
   title,
   helmet,
+  id,
+  amr,
 }) => {
   const PostContent = contentComponent || Content
+  const { edges: posts } = amr
+  console.log("amr:" + amr);
+  console.log("edges:" + posts);
+  console.log("id: " + id);
+  const myPost = posts.find((v) => v.node.id === id);
+  console.log("myPost: " + myPost);
+  const myIndex = posts.indexOf(myPost);
+  console.log(myIndex);
+  const maxIndex = posts.length;
 
   return (
     <section className="section">
@@ -26,9 +38,8 @@ export const BlogPostTemplate = ({
               {title}
             </h1>
             <p>{description}</p>
-            <PostContent content={content} />
             {tags && tags.length ? (
-              <div style={{ marginTop: `4rem` }}>
+              <div style={{ marginTop: `3rem` }}>
                 <h4>Tags</h4>
                 <ul className="taglist">
                   {tags.map(tag => (
@@ -39,11 +50,25 @@ export const BlogPostTemplate = ({
                 </ul>
               </div>
             ) : null}
+            <PostContent content={content} />
+            <h1 />
+            <div width="100%">
+              {myIndex>0 &&
+                <Link className="button" to={posts[myIndex-1].node.fields.slug}>
+                  ← 前の記事
+                </Link>}
+              {myIndex<maxIndex-1 &&
+                <Link className="button" to={posts[myIndex+1].node.fields.slug}>
+                  次の記事 →
+                </Link>}
+            </div>
+            
           </div>
         </div>
       </div>
     </section>
   )
+  
 }
 
 BlogPostTemplate.propTypes = {
@@ -52,10 +77,12 @@ BlogPostTemplate.propTypes = {
   description: PropTypes.string,
   title: PropTypes.string,
   helmet: PropTypes.object,
+  posts: PropTypes.array,
 }
 
 const BlogPost = ({ data }) => {
   const { markdownRemark: post } = data
+  const { allMarkdownRemark: AMR } = data
 
   return (
     <Layout>
@@ -74,6 +101,8 @@ const BlogPost = ({ data }) => {
         }
         tags={post.frontmatter.tags}
         title={post.frontmatter.title}
+        id={post.id}
+        amr={AMR}
       />
     </Layout>
   )
@@ -82,6 +111,9 @@ const BlogPost = ({ data }) => {
 BlogPost.propTypes = {
   data: PropTypes.shape({
     markdownRemark: PropTypes.object,
+    allMarkdownRemark: PropTypes.shape({
+      edges: PropTypes.array,
+    }),
   }),
 }
 
@@ -89,6 +121,33 @@ export default BlogPost
 
 export const pageQuery = graphql`
   query BlogPostByID($id: String!) {
+    allMarkdownRemark(
+      sort: { order: DESC, fields: [frontmatter___date] }
+      filter: { frontmatter: { templateKey: { eq: "blog-post" } } }
+    ) {
+      edges {
+        node {
+          excerpt(pruneLength: 400)
+          id
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            templateKey
+            date(formatString: "MMMM DD, YYYY")
+            featuredpost
+            featuredimage {
+              childImageSharp {
+                fluid(maxWidth: 120, quality: 100) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+        }
+      }
+    }
     markdownRemark(id: { eq: $id }) {
       id
       html
