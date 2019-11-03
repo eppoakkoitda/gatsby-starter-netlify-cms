3ac8c6396b0ddd45431670080b2976c683315708
eppoakkoitda <43262437+eppoakkoitda@users.noreply.github.com>
2019-11-03 17:30:55 +0900

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
       'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
   },
   plugins: [
+    
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
     {
@@ -73,5 +74,11 @@ module.exports = {
       },
     }, // must be after other CSS plugins
     'gatsby-plugin-netlify', // make sure to keep it last in the array
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: 'UA-151408038-1'
+      }
+    },
   ],
 }
